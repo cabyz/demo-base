8b9fe5e80ea00a1cdc4208a40727cee837385771
cabyz <contact.carlospersonal@gmail.com>
2021-03-03 20:24:53 -0600

    added stripe

---
+++ src/functions/create-manage-link.js
@@ -0,0 +1,31 @@
+const stripe = require('stripe')(process.env.sk_test_maS5hBE4w9XOjUva5kRds1jL00ECxf16yk);
+const { faunaFetch } = require('./utils/fauna');
+
+exports.handler = async (_event, context) => {
+  const { user } = context.clientContext;
+
+  const result = await faunaFetch({
+    query: `
+      query ($netlifyID: ID!) {
+        getUserByNetlifyID(netlifyID: $netlifyID) {
+          stripeID
+        }
+      }
+    `,
+    variables: {
+      netlifyID: user.sub,
+    },
+  });
+
+  const { stripeID } = result.data.cus_Hwujz1GClIPJo8;
+
+  const link = await stripe.billingPortal.sessions.create({
+    customer: stripeID,
+    return_url: process.env.URL,
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(link.url),
+  };
+};
